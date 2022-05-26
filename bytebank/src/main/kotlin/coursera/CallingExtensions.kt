d0336c88b2f7b36fe1ae9bdd6620d8e59dd8447a
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-26 10:56:50 -0300

    add new class

---
+++ bytebank/src/main/kotlin/coursera/CallingExtensions.kt
@@ -0,0 +1,11 @@
+open class Parent
+class Child: Parent()
+
+fun Parent.foo() = "parent"
+fun Child.foo() = "child"
+
+fun main(args: Array<String>) {
+    val parent: Parent = Child()
+    println(parent.foo())
+}
+
---
+++ bytebank/src/main/kotlin/coursera/CallingExtensionsGET.kt
@@ -0,0 +1,7 @@
+package coursera
+
+fun String.get(index: Int) = '*'
+
+fun main(args: Array<String>) {
+  println("abc".get(1))
+}
