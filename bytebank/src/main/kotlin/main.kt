d663e3dc5eeb5038667bc27fb00f1509bf0e583b
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-26 10:59:01 -0300

    add more class

---
+++ bytebank/src/main/kotlin/main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Bem vindo ao ByteBank")
+    /*val luiz = Funcionario(
+        nome = "Luiz",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("nome ${luiz.nome}")
+    println("cpf ${luiz.cpf}")
+    println("salario ${luiz.salario}")
+    println("bonificação ${luiz.bonificacao()}")*/
+
+//    testaComportamentosConta()
+}
