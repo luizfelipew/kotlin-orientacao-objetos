7a091a350947f5f2175b39ea834e97950c66b6d4
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2022-05-27 11:49:50 -0300

    herdando de funcionario

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -30,4 +30,27 @@ fun main() {
     } else {
         println("falha na autenticação")
     }
+
+    println("#################################")
+
+
+    val gui = Diretor(
+        nome = "Fran",
+        cpf = "333.333.333-33",
+        salario = 4000.0,
+        senha = 4000,
+        plr = 200.0
+    )
+
+    println("nome ${gui.nome}")
+    println("cpf ${gui.cpf}")
+    println("salario ${gui.salario}")
+    println("bonificação ${gui.bonificacao()}")
+    println("plr ${gui.plr}")
+
+    if (gui.autenticao(4000)) {
+        println("autenticou com sucesso como diretor")
+    } else {
+        println("falha na autenticação")
+    }
 }
