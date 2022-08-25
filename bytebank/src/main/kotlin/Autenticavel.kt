80f9970cf9976b977b91b7fe86741e80b8a15c69
Luiz Felipe <luizfelipewendt@gmail.com>
2022-08-25 20:06:38 -0300

    conhecendo mais possibilidades com interfaces

--- bytebank/src/main/kotlin/Autenticavel.kt
+++ bytebank/src/main/kotlin/Autenticavel.kt
@@ -1,4 +1,4 @@
 interface Autenticavel {
 
-    fun autentica(senha: Int) : Boolean
+    fun autentica(senha: Int): Boolean
 }
--- bytebank/src/main/kotlin/Cliente.kt
+++ bytebank/src/main/kotlin/Cliente.kt
@@ -1,8 +1,8 @@
 class Cliente(
     val nome: String,
     val cpf: String,
-    val senha: Int
-): Autenticavel {
+    private val senha: Int
+) : Autenticavel {
     override fun autentica(senha: Int): Boolean {
         if (this.senha == senha) {
             return true
